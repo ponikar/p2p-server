616d1878156d87c53eb168a7bfb40494726575b8
Darshan Ponikar <drsofficial3k@gmail.com>
2021-08-01 21:32:22 +0530

    working with broadcasting events

--- index.ts
+++ index.ts
@@ -1,15 +1,12 @@
-
 import { Socket } from "socket.io";
 import { app, io, onSocketConnection } from "./app/app";
 import { roomRouter } from "./app/routes/rooms.routes";
 
-
-onSocketConnection((socket : Socket) => {
-    console.log("LOOKS LIKE SOMEONE JOINED THE MEETING")
-    socket.on("user-joined", (e) => {
-        console.log("I got meessage for him!", e);
-    })
+onSocketConnection((socket: Socket) => {
+  socket.on("user-joined", (e) => {
+    const { meetingId } = JSON.parse(e);
+    socket.broadcast.emit(`room-${meetingId}`, e);
+  });
 });
-
 
 app.use("/", roomRouter);
