a15a9625b9db4636cd7cd9bda0c27eff4ac05c06
Darshan Ponikar <drsofficial3k@gmail.com>
2021-08-24 19:24:56 +0530

    Serving Static assets, testing for production

--- app/config/db.ts
+++ app/config/db.ts
@@ -2,9 +2,11 @@ import mongo from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongo.connect("mongodb://localhost:27017/p2p", { useNewUrlParser: true });
+    await mongo.connect("mongodb://localhost:27017/p2p", {
+      useNewUrlParser: true,
+    });
     console.log("DB CONNECTED");
   } catch (e) {
-    console.log("FAILED TO CONNECT");
+    console.log("FAILED TO CONNECT", e.message);
   }
 };
--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 import { Socket } from "socket.io";
 import { app, io, onSocketConnection } from "./app/app";
 import { roomRouter } from "./app/routes/rooms.routes";
-
+import path from "path";
+import express from "express";
 onSocketConnection((socket: Socket) => {
   socket.on("user-joined", (e) => {
-    const { meetingId } = JSON.parse(e);
+    const { meetingId, type } = JSON.parse(e);
+    console.log(type);
     socket.broadcast.emit(`room-${meetingId}`, e);
   });
+});
+
+app.use(
+  "/assets",
+  express.static(path.join(__dirname, "../p2p-client/dist/assets"))
+);
+app.get("*", (req, res) => {
+  return res.sendFile(path.join(__dirname, "../p2p-client/dist/index.html"));
 });
 
 app.use("/", roomRouter);
